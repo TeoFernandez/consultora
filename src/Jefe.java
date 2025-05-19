f3251d64487d0fbd4d97bfae84fae5ee168cdab1
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-19 00:35:48 -0300

    documentacion

--- src/Jefe.java
+++ src/Jefe.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class Jefe extends Persona{
+    int id_jefe;
+    String departamento_jefe;
 
-public class Jefe {
+    public Jefe(int id, String dni, String nombre, String apellido, String domicilio, String telefono, int id_jefe, String departamento_jefe) {
+        super(id, dni, nombre, apellido, domicilio, telefono);
+        this.id_jefe = id_jefe;
+        this.departamento_jefe = departamento_jefe;
+    }
+
+    //Geters and Seters
+    public int getId_jefe() {
+        return id_jefe;
+    }
+
+    public void setId_jefe(int id_jefe) {
+        this.id_jefe = id_jefe;
+    }
+
+    public String getDepartamento_jefe() {
+        return departamento_jefe;
+    }
+
+    public void setDepartamento_jefe(String departamento_jefe) {
+        this.departamento_jefe = departamento_jefe;
+    }
 }
